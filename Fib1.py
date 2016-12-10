dad046ff01263d048e0cb694439dc136ca4b669c
J-denoo <jdenoo@hotmail.com>
2016-12-09 21:13:38 -0500

    Update Fib1.py
    Fibonacci function

--- Fib1.py
+++ Fib1.py
@@ -1,9 +1,9 @@
-def fib_j(n):   # return Fibonacci series up to n
+def fib(n):   # return Fibonacci series up to n
 	if 𝑛 ≤ 1:
 		return 𝑛
 	else:
 		result=[]
-		result.append(0)
+		result.append(0)        ## starts with 0 and 1
 		result.append(1)
 		[result.append(result[-1]+result[-2]) for i in range(2, n)]
 		return result
